43b2bd1892883271e36ee31ade76c303f62aca77
bohyun87 <kkokko4547@gmail.com>
2023-04-25 09:27:26 +0900

    프로젝트

--- js/index.js
+++ js/index.js
@@ -14,64 +14,79 @@
 //   }, 3000);
 // });
 
-(function () {
-  const nowmovieURL =
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR&region=KR";
-
+// 현재 상영작 ajax 시작 ------------------------------------
+(function(){
+  const now_movieURL =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR&region=KR";
+  
   $.ajax({
-    type: "GET",
-    url: nowmovieURL, // 선언한 주소 변수
-    dataType: "json",
-    async: false,
-    success: function (data) {
-      const results = data.results;
-
-      for (const result of results) {
-        let imgURL = "https://image.tmdb.org/t/p/w500" + result.poster_path;
-
-        $(".now__body").append(
-          `<li class="now_list"><h6 class="now-title">${result.title}</h6>
-                <a href="./detail.html?id=${result.id}"><img class="now-img" src="${imgURL}" alt="현재상영작">
-                </li>`
-        );
-      }
-    },
-    error: function (request, status, error) {
-      console.log("code:" + request.status);
-      console.log("message:" + request.responseText);
-      console.log("error:" + error);
-    },
+  type: "GET",
+  url: now_movieURL, // 선언한 주소 변수
+  dataType: "json",
+  async: false,
+  success: function (data) {
+    const results = data.results;
+  
+    for (const result of results) {
+      let imgURL = "https://image.tmdb.org/t/p/w500" + result.poster_path;
+  
+  
+      $(".now__body").append(
+        `<li class="now_list">
+        <h6 class="now-title">${result.title}</h6>
+        <a href="./detail.html?id=${result.id}"><img class="now-img" src="${imgURL}">
+        </li>`
+      );
+    }  
+  },
+  error: function (request, status, error) {
+    console.log("code:" + request.status);
+    console.log("message:" + request.responseText);
+    console.log("error:" + error);
+  }
   });
 })();
+//현재상영작 ajax 끝----------------------------------
 
-(function () {
-  const ratedmovieURL =
-    "https://api.themoviedb.org/3/movie/popular?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR&region=KR";
-
+//영화랭킹 ajax 시작 ---------------------------------
+(function(){
+  const rank_movieURL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR";
+  
   $.ajax({
-    type: "GET",
-    url: ratedmovieURL, // 선언한 주소 변수
-    dataType: "json",
-    async: false,
-    success: function (data) {
-      const results = data.results;
-
-      let i = 1;
-      for (const result of results) {
-        if (i <= 10) {
-          $(".table_content").append(`
-          <li class="c_ranking">"i"</li>
-          <li class="c_movie_title">${result.title}</li>
-          <li class="c_movie_share">${}</li>
-          <li class="c_movie_audience">${}</li>`);
-        }
-        i++;
+  type: "GET",
+  url: rank_movieURL, // 선언한 주소 변수
+  dataType: "json",
+  async: false,
+  success: function (data) {
+    const results = data.results;
+  
+    let i = 1;
+    for (const result of results) {
+      if (i <= 10) {
+        $(".table_content").append(`
+        <li class="c_ranking">"i"</li>
+        <li class="c_movie_title">${result.title}</li>
+        <li class="c_movie_share">${}</li>
+        <li class="c_movie_audience">${}</li>`);
       }
-    },
-    error: function (request, status, error) {
-      console.log("code:" + request.status);
-      console.log("message:" + request.responseText);
-      console.log("error:" + error);
-    },
+      i++;
+    }
+
+      // let i = result.title;
+      // let title = result[i].title;
+      // console.log("영화제목: ", title);
+  
+      // $(".r_movie_title").append(`${result.title}`);
+
+    
+     
+  },
+  error: function (request, status, error) {
+    console.log("code:" + request.status);
+    console.log("message:" + request.responseText);
+    console.log("error:" + error);
+  }
   });
 })();
+//영화랭킹 ajax 끝----------------------------------
