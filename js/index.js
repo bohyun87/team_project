c8dcfe937c3196f96479a0be1088d9e89c2845c6
bohyun87 <kkokko4547@gmail.com>
2023-04-26 03:06:33 +0900

    프로젝트

--- js/index.js
+++ js/index.js
@@ -1,95 +1,87 @@
 // Team 5 Project index js
 
-
 // 이미지 슬라이드 시작 =================================================
-jQuery(document).ready(function () {
-  setInterval (function () {
-    $('.slidelist > li').delay(1000);
-    $('.slidelist > li').animate({ marginLeft: "-100%" });
-    $('.slidelist > li').delay(2000);
-    $('.slidelist > li').animate({ marginLeft: "-200%" });
-    $('.slidelist > li').delay(2000);
-    $('.slidelist > li').animate({ marginLeft: "-300%" });
-    $('.slidelist > li').delay(2000);
-    $('.slidelist > li').animate({ marginLeft: 0 });
-    $('.slidelist > li').delay(1000);
-  }, 3000);
-});
+
+$(document).ready(function () {
+  $('.imgslide a:gt(0)').hide();
+  setInterval(function(){
+    $('.imgslide a:first-child')
+    .fadeOut()
+    .next('a')
+    .fadeIn()
+    .next('a')
+    .fadeOut()
+    .end()
+    .appendTo('.imgslide')
+  },4000);
+}); 
 // 이미지 슬라이드 끝 ==================================================
 
 // 현재 상영작 ajax 시작 ------------------------------------
-(function(){
-  const nowmovieURL =
+const now_movieURL =
   "https://api.themoviedb.org/3/movie/now_playing?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR&region=KR";
-  
-  $.ajax({
+
+$.ajax({
   type: "GET",
-  url: nowmovieURL, // 선언한 주소 변수
+  url: now_movieURL,
   dataType: "json",
   async: false,
   success: function (data) {
     const results = data.results;
-  
+
     for (const result of results) {
       let imgURL = "https://image.tmdb.org/t/p/w500" + result.poster_path;
-  
-  
-      $(".now__body").append(
+            $(".now__body").append(
         `<li class="now_list">
-        <h6 class="now-title">${result.title}</h6>
-        <a href="./detail.html?id=${result.id}"><img class="now-img" src="${imgURL}">
-        </li>`
+           <a href="./detail.html?id=${result.id}">
+           <img class="now-img" src="${imgURL}">
+           <span class="now-title">${result.title}</span>
+           </a>
+         </li>`
       );
-    }  
+    }
   },
   error: function (request, status, error) {
     console.log("code:" + request.status);
     console.log("message:" + request.responseText);
     console.log("error:" + error);
-  }
-  });
-})();
+  },
+});
 //현재상영작 ajax 끝----------------------------------
 
 //영화랭킹 ajax 시작 ---------------------------------
-(function(){
+(function () {
   const rank_movieURL =
-  "https://api.themoviedb.org/3/movie/popular?api_key=c8509b8f85db6c465df670fd3eb60def&language=ko-KR";
-  
+    "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=4cb53f1391db2b7155f3fdd51d5a894d&targetDt=20230421&weekGb=0";
+
   $.ajax({
-  type: "GET",
-  url: rank_movieURL, // 선언한 주소 변수
-  dataType: "json",
-  async: false,
-  success: function (data) {
-    const results = data.results;
-  
-    let i = 1;
-    for (const result of results) {
-      if (i <= 10) {
-        $(".table_content").append(`
-        <li class="c_ranking">"i"</li>
-        <li class="c_movie_title">${result.title}</li>
-        <li class="c_movie_share">${}</li>
-        <li class="c_movie_audience">${}</li>`);
+    type: "GET",
+    url: rank_movieURL, // 선언한 주소 변수
+    dataType: "json",
+    async: false,
+    success: function (data) {
+      const lists = data.boxOfficeResult.weeklyBoxOfficeList;
+      console.log(lists);
+
+      let i = 1;
+      for (const list of lists) {
+        if (i <= 10) {
+          $(".table_content").append(`
+          <ul class="table_content_list">
+            <li class="c_ranking col-md-2">${list.rank}</li>
+            <li class="c_movie_title col-md-4">${list.movieNm}</li>
+            <li class="c_movie_share col-md-3">${list.salesShare}</li>
+            <li class="c_movie_audience col-md-3">${list.audiCnt}</li>
+          </ul>`);
+        }
+        i++;
       }
-      i++;
-    }
-
-      // let i = result.title;
-      // let title = result[i].title;
-      // console.log("영화제목: ", title);
-  
-      // $(".r_movie_title").append(`${result.title}`);
-
-    
-     
-  },
-  error: function (request, status, error) {
-    console.log("code:" + request.status);
-    console.log("message:" + request.responseText);
-    console.log("error:" + error);
-  }
+    },
+    error: function (request, status, error) {
+      console.log("code:" + request.status);
+      console.log("message:" + request.responseText);
+      console.log("error:" + error);
+    },
   });
 })();
 //영화랭킹 ajax 끝----------------------------------
